dbf5ad48ba5f8d4f4e9ad0a61404ad64c747e59e
samuelv2umc <11221103351@alunos.umc.br>
2025-09-30 20:13:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
